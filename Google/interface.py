f691697c0d216ae8788011126c9eccf44744fa4c
Kappeh <kieranleep@hotmail.com>
2018-12-30 17:15:01 +0000

    Fixed google authentication.

--- Google/interface.py
+++ Google/interface.py
@@ -1,41 +1,32 @@
 import os
 import sys
+import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 # Establishing connection with google APIs
 def connect():
-    scope = [
+    scopes = [
         'https://spreadsheets.google.com/feeds',
         'https://www.googleapis.com/auth/drive'
     ]
 
     credentials = None
-    if os.path.isfile('Google/client_secret.json'):
-        # Getting service account credentials from json file
-        credentials = ServiceAccountCredentials.from_json_keyfile_name('Google/client_secret.json', scope)
-    else:
+
+    if not os.path.isfile('Google/client_secret.json'):
         # Getting service account credentials from environment variables
-        credentials = {
-            "type": os.environ.get('GOOGLE_TYPE'),
-            "project_id": os.environ.get('GOOGLE_PROJECT_ID'),
-            "private_key_id": os.environ.get('GOOGLE_PRIVATE_KEY_ID'),
-            "private_key": os.environ.get('GOOGLE_PRIVATE_KEY'),
-            "client_email": os.environ.get('GOOGLE_CLIENT_EMAIL'),
-            "client_id": os.environ.get('GOOGLE_CLIENT_ID'),
-            "auth_uri": os.environ.get('GOOGLE_AUTH_URI'),
-            "token_uri": os.environ.get('GOOGLE_TOKEN_URI'),
-            "auth_provider_x509_cert_url": os.environ.get('GOOGLE_AUTH_PROVIDER_X509_CERT_URL'),
-            "client_x509_cert_url": os.environ.get('GOOGLE_CLIENT_X509_CERT_URL')
-        }
+        credentials = os.environ.get('GOOGLE_CREDENTIALS')
         
         # Checking environment variables exist
-        for element in credentials.keys():
-            if not credentials[element]:
-                raise Exception('Specify google credentials with a client_secret.json or environment variables.')
+        if not credentials:
+            raise Exception('Specify google credentials with a client_secret.json or environment variables.')
 
-        # Formatting json into service account credentials object
-        credentials = ServiceAccountCredentials.from_json(credentials)
+        # Formatting credentials
+        credentials = json.loads(credentials)
+        credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials, scopes)
+    else:
+        # Getting service account credentials from json file
+        credentials = ServiceAccountCredentials.from_json_keyfile_name('Google/client_secret.json', scopes)
 
     return gspread.authorize(credentials)
 
