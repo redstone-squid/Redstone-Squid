c7e718f43107ab1f7549fe6bf1ad3208f2d932c7
Kappeh <kieranleep@hotmail.com>
2019-01-15 00:50:04 +0000

    Created interface for global variables in database

---
+++ Database/global_vars.py
@@ -0,0 +1,18 @@
+import Database.main as DB
+import Database.config as config
+import Google.interface as Google
+
+def get_next_id():
+    wks = DB.get_globals_worksheet()
+    return int(wks.get_all_records()[0]['Next ID'])
+
+def update_next_id(new_next_id):
+    wks = DB.get_globals_worksheet()
+    col_num = DB.get_col_number(wks, 'Next ID')
+    wks.update_cell(2, col_num, new_next_id)
+
+def add_to_next_id(count):
+    wks = DB.get_globals_worksheet()
+    next_id = int(wks.get_all_records()[0]['Next ID']) + count
+    col_num = DB.get_col_number(wks, 'Next ID')
+    wks.update_cell(2, col_num, next_id)
