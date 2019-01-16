ae4549ae0ac80d08b22ca6da128cd0d44bf5559b
Kappeh <kieranleep@hotmail.com>
2019-01-16 03:03:20 +0000

    Created helper functions for managing database

--- Database/submissions.py
+++ Database/submissions.py
@@ -4,6 +4,8 @@ import Database.global_vars as global_vars
 import Database.config as config
 import Google.interface as Google
 
+import Database.submission as submission
+
 # Moves all records from the form_submissions worksheet to the open submissions worksheet.
 def open_form_submissions():
     # Getting worksheets
@@ -64,3 +66,47 @@ def get_open_submissions():
         submissions[index] = Submission_Class.from_dict(submission)
 
     return submissions
+
+def get_open_submission(submission_id):
+    return get_submission(submission_id, DB.get_open_submissions_worksheet())
+
+# Returns a Submission object parsed from wks with the submission_id
+def get_submission(submission_id, wks):
+    submissions = wks.get_all_records()
+    for sub in submissions:
+        if int(sub['Submission ID']) == submission_id:
+            return submission.Submission.from_dict(sub)
+    return None
+
+# Moves submission with submission_id from source_wks to destination_wks
+# Returns if the submission exists in source_wks
+def move_submission(submission_id, source_wks, destination_wks):
+    # Gets all submission from source
+    submissions = source_wks.get_all_records()
+
+    # Finds the row number of the submission
+    row_number = None
+    for index, sub in enumerate(submissions):
+        if int(sub['Submission ID']) == submission_id:
+            row_number = index + 2
+            break
+
+    # Returns False if no such submission exist
+    if row_number == None:
+        return False
+
+    # Appends the submission to the destination worksheet
+    destination_wks.append_row(source_wks.row_values(row_number))
+    # Removes the submission from the source worksheet
+    source_wks.delete_row(row_number)
+
+    return True
+
+# Confirm submission
+def confirm_submission(submission_id):
+    return move_submission(submission_id, DB.get_open_submissions_worksheet(), DB.get_confirmed_submissions_worksheet())
+
+# Deny submission
+def deny_submission(submission_id):
+    return move_submission(submission_id, DB.get_open_submissions_worksheet(), DB.get_denied_submissions_worksheet())
+    
