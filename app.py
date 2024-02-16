3ac548a2f8150e1ec93c9252b45f3b9933e72c14
Glinte <96855131+Glinte@users.noreply.github.com>
2024-02-16 18:26:35 +0800

    Comment to indicate main entry point better

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # RecordBot version 1.0
 # Made by Kappeh
 # https://github.com/Kappeh/RecordBot
+# This is the main file of the bot. It is responsible for starting the bot and handling the commands.
 
 import Google.interface as google
 import Discord.interface as discord
