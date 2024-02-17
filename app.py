4398aaf2242eeb1dbec6ed6d9024f60567dc0839
Glinte <96855131+Glinte@users.noreply.github.com>
2024-02-17 20:05:33 +0800

    comments

--- app.py
+++ app.py
@@ -2,6 +2,16 @@
 # Made by Kappeh
 # https://github.com/Kappeh/RecordBot
 # This is the main file of the bot. It is responsible for starting the bot and handling the commands.
+# Setup:
+# 1. Install the required libraries with `pip install -r requirements.txt` or `conda install --file environment.yml`.
+# 2. Create a Discord bot and get the token.
+# 3. Create a Google service account and get the credentials.
+# 4. Create a file called `auth.ini` in the Discord folder and add the following:
+# ```
+# [discord]
+# token = YOUR BOT TOKEN
+# ```
+# 5. Create a file called `client_secret.json` in the Google folder and add the credentials.
 
 import Google.interface as google
 import Discord.interface as discord
