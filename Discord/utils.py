0fdec68e14a9085713954aac9ff22bc54adda4ab
Glinte <96855131+Glinte@users.noreply.github.com>
2024-02-21 18:54:22 +0800

    use raw strings for regex and code quality

--- Discord/utils.py
+++ Discord/utils.py
@@ -6,12 +6,14 @@ discord_red = 0xF04747
 discord_yellow = 0xFAA61A
 discord_green = 0x43B581
 
+
 def represents_int(s):
     try:
         int(s)
         return True
     except ValueError:
         return False
+
 
 def represents_float(s):
     try:
@@ -20,25 +22,31 @@ def represents_float(s):
     except ValueError:
         return False
 
+
 def represents_user(s):
-    if re.match('<@!?\d{17,18}>', s) == None:
+    if re.match(r'<@!?\d{17,18}>', s) is None:
         return False
     return True
 
+
 def represents_channel(s):
-    if re.match('<#\d{18}>', s) == None:
+    if re.match(r'<#\d{18}>', s) is None:
         return False
     return True
+
 
 def get_time():
     raw_time = strftime("%Y/%m/%d %H:%M:%S", gmtime())
     return '[' + raw_time + '] '
 
+
 def error_embed(title, description):
-    return discord.Embed(title = title, colour = discord_red, description = ':x: ' + description)
+    return discord.Embed(title=title, colour=discord_red, description=':x: ' + description)
+
 
 def warning_embed(title, description):
-    return discord.Embed(title = ':warning: ' + title, colour = discord_yellow, description = description)
+    return discord.Embed(title=':warning: ' + title, colour=discord_yellow, description=description)
+
 
 def info_embed(title, description):
-    return discord.Embed(title = title, colour = discord_green, description = description)
+    return discord.Embed(title=title, colour=discord_green, description=description)
