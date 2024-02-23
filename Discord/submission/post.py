9c6a5b3dd31f62fd4739d0f2d23c3d664e609c4a
Glinte <96855131+Glinte@users.noreply.github.com>
2024-02-23 13:34:48 +0800

    PEP8

--- Discord/submission/post.py
+++ Discord/submission/post.py
@@ -1,4 +1,3 @@
-
 import discord
 
 import Discord.utils as utils
@@ -12,6 +11,7 @@ import Database.submission as submission
 import Database.server_settings as server_settings
 import Database.message as msg
 
+
 def generate_embed(submission_obj):
     # Title -------------------------------------------------------------------------------
     # Catagory
@@ -19,24 +19,25 @@ def generate_embed(submission_obj):
 
     # Description -------------------------------------------------------------------------
     description = submission_obj.get_description()
-    
+
     # Embed -------------------------------------------------------------------------------
     em = None
     if description is None:
-        em = discord.Embed(title = title,  colour = utils.discord_green)
+        em = discord.Embed(title=title, colour=utils.discord_green)
     else:
-        em = discord.Embed(title = title, description = description, colour = utils.discord_green)
+        em = discord.Embed(title=title, description=description, colour=utils.discord_green)
 
     fields = submission_obj.get_meta_fields()
     for key, val in fields.items():
-        em.add_field(name = key, value = val, inline = True)
-    
-    if submission_obj.image_url:
-        em.set_image(url = submission_obj.image_url)
+        em.add_field(name=key, value=val, inline=True)
 
-    em.set_footer(text = 'Submission ID: {}.'.format(submission_obj.id))
+    if submission_obj.image_url:
+        em.set_image(url=submission_obj.image_url)
+
+    em.set_footer(text='Submission ID: {}.'.format(submission_obj.id))
 
     return em
+
 
 # Get the channels ['smallest', 'fastest', 'smallest_observerless', 'fastest_observerless'] to post record to
 def get_channel_type_to_post_to(submission_obj):
@@ -57,8 +58,9 @@ def get_channel_type_to_post_to(submission_obj):
             return 'Smallest Observerless'
         else:
             return 'Smallest'
-    
+
     return None
+
 
 # Gets all channels which a submission should be posted to
 def get_channels_to_post_to(client, submission_obj):
@@ -74,10 +76,11 @@ def get_channels_to_post_to(client, submission_obj):
 
         if not channel:
             continue
-        
+
         channels.append(channel)
-    
+
     return channels
+
 
 # Posts submission to each server in the channel the server settings worksheet
 async def post_submission(client, submission_obj):
@@ -85,8 +88,9 @@ async def post_submission(client, submission_obj):
     em = generate_embed(submission_obj)
 
     for channel in channels:
-        message = await client.send_message(channel, embed = em)
+        message = await client.send_message(channel, embed=em)
         msg.update_message(channel.server.id, submission_obj.id, message.channel.id, message.id)
+
 
 async def post_submission_to_server(client, submission_obj, server_id):
     channels = get_channels_to_post_to(client, submission_obj)
@@ -94,15 +98,15 @@ async def post_submission_to_server(client, submission_obj, server_id):
 
     for channel in channels:
         if channel.server.id == server_id:
-            message = await client.send_message(channel, embed = em)
+            message = await client.send_message(channel, embed=em)
             msg.update_message(channel.server.id, submission_obj.id, message.channel.id, message.id)
+
 
 # Updates post to conform to the submission obj
 async def edit_post(client, server, channel_id, message_id, submission_obj):
-
     em = generate_embed(submission_obj)
     channel = client.get_channel(str(channel_id))
     message = await client.get_message(channel, int(message_id))
 
-    updated_message = await client.edit_message(message, embed = em)
+    updated_message = await client.edit_message(message, embed=em)
     msg.update_message(server.id, submission_obj.id, str(channel_id), updated_message.id)
