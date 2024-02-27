1cf061d5b1245fb7d6a1c03643ea5f8ea476587d
Glinte <96855131+Glinte@users.noreply.github.com>
2024-02-27 15:34:38 +0800

    bump version to 1.2.0

--- Discord/config.py
+++ Discord/config.py
@@ -3,6 +3,6 @@ OWNER_ID = 353089661175988224
 OWNER_SERVER_ID = 433618741528625152
 PREFIX = '!'
 BOT_NAME = 'Redstone Squid'
-BOT_VERSION = '1.1.1'
+BOT_VERSION = '1.2.0'
 SOURCE_CODE_URL = 'https://github.com/Kappeh/Redstone-Squid'
 FORM_LINK = 'https://forms.gle/i9Nf6apGgPGTUohr9'
