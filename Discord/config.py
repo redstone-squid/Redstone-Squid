30a4d483c70ac176c1fcc5cd2309191a42b99365
Glinte <96855131+Glinte@users.noreply.github.com>
2024-03-05 16:38:32 +0800

    Move versions_list and delete config.py in Database

--- Discord/config.py
+++ Discord/config.py
@@ -6,3 +6,6 @@ BOT_NAME = 'Redstone Squid'
 BOT_VERSION = '1.3.1'
 SOURCE_CODE_URL = 'https://github.com/Kappeh/Redstone-Squid'
 FORM_LINK = 'https://forms.gle/i9Nf6apGgPGTUohr9'
+# List of versions for version string parser
+VERSIONS_LIST = ['Pre 1.5', '1.5', '1.6', '1.7', '1.8', '1.9', '1.10', '1.11', '1.12', '1.13',
+                 '1.13.1 / 1.13.2', '1.14', '1.14.1', '1.15', '1.16', '1.17', '1.18', '1.19', '1.20', '1.20.4']
