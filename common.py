ee4d8e4934360ce909ae93ee3d0746fbafcc2888
Glinte <96855131+Glinte@users.noreply.github.com>
2024-04-11 15:46:00 +0800

    typing

--- common.py
+++ common.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from dateutil import tz
 
-def get_current_utc():
+def get_current_utc() -> str:
     current_utc = datetime.now(tz=tz.UTC)
     formatted_time = current_utc.strftime('%Y-%m-%d %H:%M:%S')
     return formatted_time
