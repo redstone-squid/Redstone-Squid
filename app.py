8e75bf11d08deb7b1b4253ed922f15d14a3dd7d0
Glinte <96855131+Glinte@users.noreply.github.com>
2024-07-07 15:29:59 +0800

    Revert dev mode

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # https://github.com/Kappeh/RecordBot
 # This is the main file of the bot. It is responsible for starting the bot and handling the commands.
 from bot import config
-config.DEV_MODE = True  # Have to be above the import of main
+
+config.DEV_MODE = False  # Have to be above the import of main
 
 import asyncio
 from bot.main import main
