0814f004c4ff0ddb6e0c369bb9c6e68683e5adc5
Glinte <96855131+Glinte@users.noreply.github.com>
2024-07-15 21:59:31 +0800

    Use new repo link

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-# https://github.com/Kappeh/RecordBot
+# https://github.com/redstone-squid/Redstone-Squid
 # This is the main file of the bot. It is responsible for starting the bot and handling the commands.
 from bot import config
 
 config.DEV_MODE = False  # Have to be above the import of main
 
-import asyncio
-from bot.main import main
+import asyncio  # noqa: E402
+from bot.main import main  # noqa: E402
 
 
 asyncio.run(main())
