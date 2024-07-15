8289708a3dfc4c2c5d9e798d6564d1754f9d252c
Glinte <96855131+Glinte@users.noreply.github.com>
2024-07-15 21:51:07 +0800

    Add docstring for utcnow

--- Database/utils.py
+++ Database/utils.py
@@ -2,6 +2,7 @@ from datetime import datetime, timezone
 
 
 def utcnow() -> str:
+    """Returns the current time in UTC in the format of a string."""
     current_utc = datetime.now(tz=timezone.utc)
     formatted_time = current_utc.strftime("%Y-%m-%dT%H:%M:%S")
     return formatted_time
