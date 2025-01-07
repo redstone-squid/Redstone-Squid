2e03ec351e6564ca093aee995367bb7d9abca1c6
Glinte <96855131+Glinte@users.noreply.github.com>
2025-01-07 22:26:25 +0800

    Add is_staff helper

--- bot/utils.py
+++ bot/utils.py
@@ -130,6 +130,21 @@ def check_is_staff():
     return check(predicate)
 
 
+async def is_staff(bot: discord.Client, server_id: int, user_id: int) -> bool:
+    """Check if the user has a staff role, as defined in the server settings."""
+    staff_role_ids = await get_server_setting(server_id=server_id, setting="Staff")
+    server = bot.get_guild(server_id)
+    if server is None:
+        return False
+    member = server.get_member(user_id)
+    if member is None:
+        return False
+
+    if set(staff_role_ids) & set(role.id for role in member.roles):
+        return True
+    return False
+
+
 def check_is_trusted_or_staff():
     """Check if the user has a trusted or staff role, as defined in the server settings."""
 
