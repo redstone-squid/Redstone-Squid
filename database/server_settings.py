4fde6501692f1f1ac5bd1b61ad32122f436cafea
Glinte <96855131+Glinte@users.noreply.github.com>
2025-01-07 14:24:15 +0800

    Inline get_setting_name

--- database/server_settings.py
+++ database/server_settings.py
@@ -30,11 +30,6 @@ _DB_KEY_TO_SETTING: dict[DbSettingKey, Setting] = {value: key for key, value in 
 assert set(_SETTING_TO_DB_KEY.keys()) == set(SETTINGS), "The mapping is not exhaustive!"
 
 
-def get_setting_name(setting: Setting) -> DbSettingKey:
-    """Maps a setting to the column name in the database."""
-    return _SETTING_TO_DB_KEY[setting]
-
-
 @overload
 async def get_server_setting(server_id: int, setting: Literal["Smallest", "Fastest", "First", "Builds", "Vote"]) -> int | None: ...
 @overload
@@ -49,18 +44,18 @@ async def get_server_setting(server_id: int, setting: Setting) -> int | list[int
 
     The returned channel ids are always a ``GuildMessageable``.
     """
-    setting_name = get_setting_name(setting)
+    col_name = _SETTING_TO_DB_KEY[setting]
     response: SingleAPIResponse[ServerSettingRecord] | None = (
         await DatabaseManager()
         .table("server_settings")
-        .select(setting_name, count=CountMethod.exact)
+        .select(col_name, count=CountMethod.exact)
         .eq("server_id", server_id)
         .maybe_single()
         .execute()
     )
     if response is None:
         return None
-    return response.data.get(setting_name)
+    return response.data.get(col_name)
 
 
 async def get_server_settings(server_id: int) -> dict[Setting, int | list[int] | None]:
@@ -86,11 +81,11 @@ async def update_server_setting(server_id: int, setting: Setting, value: int | l
 
 async def update_server_setting(server_id: int, setting: Setting, value: int | list[int] | None) -> None:
     """Updates a setting for a server."""
-    setting_name = get_setting_name(setting)
-    await DatabaseManager().table("server_settings").upsert({"server_id": server_id, setting_name: value}).execute()
+    col_name = _SETTING_TO_DB_KEY[setting]
+    await DatabaseManager().table("server_settings").upsert({"server_id": server_id, col_name: value}).execute()
 
 
 async def update_server_settings(server_id: int, settings: dict[Setting, int | list[int] | None]) -> None:
     """Updates a list of settings for a server."""
-    db_cols_mapping = {get_setting_name(purpose): value for purpose, value in settings.items()}
-    await DatabaseManager().table("server_settings").upsert({"server_id": server_id, **db_cols_mapping}).execute()
+    data = {_SETTING_TO_DB_KEY[purpose]: value for purpose, value in settings.items()}
+    await DatabaseManager().table("server_settings").upsert({"server_id": server_id, **data}).execute()
