affece5f8a3e72805063cd74ef4692372c9f1577
Glinte <96855131+Glinte@users.noreply.github.com>
2025-01-07 13:10:08 +0800

    Fix /settings not supporting changing staff and trusted

--- bot/settings.py
+++ bot/settings.py
@@ -2,11 +2,11 @@
 
 from __future__ import annotations
 
-from typing import cast, TYPE_CHECKING
+from typing import cast, TYPE_CHECKING, Annotated
 
 import discord
 from discord import app_commands
-from discord.ext.commands import Context, Cog, hybrid_group, guild_only
+from discord.ext.commands import Context, Cog, hybrid_group, guild_only, Greedy
 from postgrest.types import ReturnMethod
 
 from bot.utils import check_is_staff
@@ -22,8 +22,6 @@ from bot._types import GuildMessageable
 
 if TYPE_CHECKING:
     from bot.main import RedstoneSquid
-
-# TODO: Make all commands in this cog guild only
 
 
 class SettingsCog(Cog, name="Settings"):
@@ -55,96 +53,125 @@ class SettingsCog(Cog, name="Settings"):
     @settings_hybrid_group.command(name="list")
     @check_is_staff()
     async def show_server_settings(self, ctx: Context):
-        """Query all settings."""
+        """Show all settings for this server."""
         assert ctx.guild is not None
         async with utils.RunningMessage(ctx) as sent_message:
             settings = await get_server_settings(ctx.guild.id)
-            channels: dict[Setting, GuildMessageable | None] = {}
-            for record_type in SETTINGS:
-                channel_id = settings.get(record_type)
-                if channel_id is None:
-                    continue
-                channels[record_type] = cast(GuildMessageable | None, ctx.guild.get_channel(channel_id))
-
             desc = ""
-            for channel_type in SETTINGS:
-                desc += f"`{channel_type.lower()} channel`: {channels.get(channel_type, '_Not set_')}\n"
+            for setting, value in settings.items():
+                match setting:
+                    case "Smallest" | "Fastest" | "First" | "Builds" | "Vote":
+                        value = cast(int | None, value)
+                        if value is None:
+                            desc += f"{setting} channel: _Not set_\n"
+                            continue
+                        channel = cast(GuildMessageable | None, ctx.guild.get_channel(value))
+                        display_value = channel.name if channel is not None else "_Not found_"
+                        desc += f"{setting} channel: {display_value}\n"
+                    case "Staff" | "Trusted":
+                        value = cast(list[int], value)
+                        roles = [role for role in ctx.guild.roles if role.id in value]
+                        display_value = ", ".join(role.name for role in roles) or "_Not set_"
+                        desc += f"{setting} roles: {display_value}\n"
+                    case _:  # pyright: ignore[reportUnnecessaryComparison]  # Should not happen, but may happen if the schema is updated and this code is not
+                        desc += f"{setting}: {value}\n"
 
             await sent_message.edit(embed=utils.info_embed(title="Current Settings", description=desc))
 
     @settings_hybrid_group.command(name="search")
-    @app_commands.describe(channel_purpose=", ".join(SETTINGS))
-    @app_commands.rename(channel_purpose="type")
+    @app_commands.describe(setting=", ".join(SETTINGS))
+    @app_commands.rename(setting="type")
     @check_is_staff()
-    async def search_setting(self, ctx: Context[RedstoneSquid], channel_purpose: Setting):
+    async def search_setting(self, ctx: Context[RedstoneSquid], setting: Setting):
         """Show the server's current setting."""
         assert ctx.guild is not None
-        unset_em = utils.info_embed(
-            f"{channel_purpose} Channel Info",
-            "Unset - Use the set command to set a channel.",
-        )
+
+        title: str
+        description: str
         async with utils.RunningMessage(ctx) as sent_message:
-            channel_id = await get_server_setting(ctx.guild.id, channel_purpose)
-            if not channel_id:
-                await sent_message.edit(embed=unset_em)
-                return
+            match setting:
+                case "Smallest" | "Fastest" | "First" | "Builds" | "Vote":
+                    title = f"{setting} Channel Info"
+                    value = await get_server_setting(ctx.guild.id, setting)
+                    if value is None:
+                        description = "_Not set_"
+                    else:
+                        channel = ctx.guild.get_channel(value)
+                        description = f"ID: {channel.id} \n Name: {channel.name}" if channel is not None else "_Not found_"
+                case "Staff" | "Trusted":
+                    title = f"{setting} Roles Info"
+                    value = await get_server_setting(ctx.guild.id, setting)
+                    if value is None:
+                        description = "_Not set_"
+                    else:
+                        roles = [role for role in ctx.guild.roles if role.id in value]
+                        description = ", ".join(role.name for role in roles) or "_Not set_"
+                case _:  # pyright: ignore[reportUnnecessaryComparison]  # Should not happen, but may happen if the schema is updated and this code is not
+                    title = setting
+                    description = str(get_server_setting(ctx.guild.id, setting))
 
-            result_channel = ctx.bot.get_channel(channel_id)
-            if result_channel is None:
-                await sent_message.edit(embed=unset_em)
-                return
-
-            assert isinstance(result_channel, GuildMessageable)
-            em = utils.info_embed(
-                f"{channel_purpose} Channel Info",
-                f"ID: {result_channel.id} \n Name: {result_channel.name}",
-            )
-            await sent_message.edit(embed=em)
+            await sent_message.edit(embed=utils.info_embed(title=title, description=description))
 
     @settings_hybrid_group.command(name="set")
     @app_commands.describe(
-        channel_purpose=", ".join(SETTINGS),
+        setting=", ".join(SETTINGS),
         channel="The channel that you want to set to send this record type to.",
     )
-    @app_commands.rename(channel_purpose="type")
+    @app_commands.rename(setting="type")
     @check_is_staff()
     async def change_setting(
         self,
         ctx: Context,
-        channel_purpose: Setting,
-        channel: GuildMessageable,
+        setting: Setting,
+        channel: GuildMessageable | None = None,
+        roles: Annotated[list[discord.Role] | None, Greedy[discord.Role]] = None,
     ):
         """Change the server's setting."""
         assert ctx.guild is not None
-        success_embed = utils.info_embed("Settings updated", f"{channel_purpose} channel has successfully been set.")
-        failure_embed = utils.error_embed("Error", "Could not find that channel.")
+
+        if channel is not None and roles is not None:
+            await ctx.send(embed=utils.error_embed("Error", "You can only provide a channel or a list of roles, not both."))
+            return
 
         async with utils.RunningMessage(ctx) as sent_message:
-            # Verifying channel exists on server
-            if ctx.guild.get_channel(channel.id) is None:
-                await sent_message.edit(embed=failure_embed)
-                return
+            match setting:
+                case "Smallest" | "Fastest" | "First" | "Builds" | "Vote":
+                    if channel is None:
+                        await sent_message.edit(embed=utils.error_embed("Error", "You must provide a channel for this setting."))
+                        return
 
-            # Updating database
-            # TODO: Add a check when adding channels to the database to make sure they are GuildMessageable
-            await update_server_setting(ctx.guild.id, channel_purpose, channel.id)
-            await sent_message.edit(embed=success_embed)
+                    if ctx.guild.get_channel(channel.id) is None:
+                        await sent_message.edit(embed=utils.error_embed("Error", "Could not find that channel."))
+                        return
+
+                    # TODO: Add a check when adding channels to the database to make sure they are GuildMessageable
+                    await update_server_setting(ctx.guild.id, setting, channel.id)
+                    await sent_message.edit(embed=utils.info_embed("Settings updated", f"{setting} channel has successfully been set."))
+                case "Staff" | "Trusted":
+                    if roles is None:
+                        await sent_message.edit(embed=utils.error_embed("Error", "You must provide a list of roles for this setting."))
+                        return
+
+                    role_ids = [role.id for role in roles]
+                    if any(role.guild != ctx.guild for role in roles):
+                        await sent_message.edit(embed=utils.error_embed("Error", "The roles must be from this server."))
+                        return
+
+                    await update_server_setting(ctx.guild.id, setting, role_ids)
+                case _:  # pyright: ignore[reportUnnecessaryComparison]  # Should not happen, but may happen if the schema is updated and this code is not
+                    await sent_message.edit(embed=utils.error_embed("Error", "This setting is not supported."))
 
     @settings_hybrid_group.command(name="clear")
-    @app_commands.describe(channel_purpose=", ".join(SETTINGS))
-    @app_commands.rename(channel_purpose="type")
+    @app_commands.describe(setting=", ".join(SETTINGS))
+    @app_commands.rename(setting="type")
     @check_is_staff()
-    async def clear_setting(self, ctx: Context, channel_purpose: Setting):
+    async def clear_setting(self, ctx: Context, setting: Setting):
         """Set this setting to None."""
         assert ctx.guild is not None
-        success_embed = utils.info_embed(
-            "Settings updated",
-            f"{channel_purpose} channel has successfully been unset.",
-        )
 
         async with utils.RunningMessage(ctx) as sent_message:
-            await update_server_setting(ctx.guild.id, channel_purpose, None)
-            await sent_message.edit(embed=success_embed)
+            await update_server_setting(ctx.guild.id, setting, None)
+            await sent_message.edit(embed=utils.info_embed("Setting updated", f"{setting} has been cleared."))
 
 
 async def setup(bot: RedstoneSquid):
