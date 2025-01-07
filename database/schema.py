ce8e93ad8bc5d0f4915c942694c868233200f72e
Glinte <96855131+Glinte@users.noreply.github.com>
2025-01-07 12:19:15 +0800

    Remove ChannelPurpose and RoleSetting
    Everything uses the Union Setting now

--- database/schema.py
+++ database/schema.py
@@ -168,14 +168,8 @@ BUILD_TYPES: Sequence[BuildType] = cast(Sequence[BuildType], get_args(BuildType)
 DoorOrientationName: TypeAlias = Literal["Door", "Skydoor", "Trapdoor"]
 DOOR_ORIENTATION_NAMES = cast(Sequence[DoorOrientationName], get_args(DoorOrientationName))
 
-ChannelPurpose: TypeAlias = Literal["Smallest", "Fastest", "First", "Builds", "Vote"]
-CHANNEL_PURPOSES = cast(Sequence[ChannelPurpose], get_args(ChannelPurpose))
-
-RoleSetting: TypeAlias = Literal["Staff", "Trusted"]
-ROLE_SETTINGS = cast(Sequence[RoleSetting], get_args(RoleSetting))
-
-Setting: TypeAlias = ChannelPurpose | RoleSetting
-SETTINGS = cast(Sequence[Setting], list(CHANNEL_PURPOSES) + list(ROLE_SETTINGS))
+Setting: TypeAlias = Literal["Smallest", "Fastest", "First", "Builds", "Vote", "Staff", "Trusted"]
+SETTINGS = cast(Sequence[Setting], get_args(Setting))
 
 Restriction = Literal["wiring-placement", "component", "miscellaneous"]
 RESTRICTIONS = cast(Sequence[Restriction], get_args(Restriction))
