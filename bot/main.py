3ae250f6dbc84e341c79ebcb41988925688e718a
Glinte <96855131+Glinte@users.noreply.github.com>
2025-01-20 14:36:50 +0800

    Move the set_event_loop_policy outside of main()

--- bot/main.py
+++ bot/main.py
@@ -65,9 +65,6 @@ async def main():
     """Main entry point for the bot."""
     prefix = PREFIX if not DEV_MODE else DEV_PREFIX
 
-    if sys.platform == 'win32':  # https://github.com/aio-libs/aiodns/issues/86
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-
     async with RedstoneSquid(command_prefix=commands.when_mentioned_or(prefix)) as bot:
         discord.utils.setup_logging()
         load_dotenv()
@@ -78,4 +75,6 @@ async def main():
 
 
 if __name__ == "__main__":
+    if sys.platform == 'win32':  # https://github.com/aio-libs/aiodns/issues/86
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     asyncio.run(main())
