40f83150486a395702e07da14c49cef3b4bd1e12
Glinte <96855131+Glinte@users.noreply.github.com>
2025-01-21 13:20:01 +0800

    Warn about the use of aiodns in Windows

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ https://github.com/redstone-squid/Redstone-Squid"
 
 import sys
 import asyncio
+import logging
+from importlib.util import find_spec
 import multiprocessing
 
 from bot.main import main
@@ -16,6 +18,8 @@ if __name__ == "__main__":
     # Check bot/config.py for configuration, .env.example for environment variables
     multiprocessing.Process(target=api_main).start()
 
-    if sys.platform == 'win32':  # https://github.com/aio-libs/aiodns/issues/86
+    if sys.platform == 'win32' and find_spec("aiodns"):  # https://github.com/Rapptz/discord.py/pull/9898 & https://github.com/aio-libs/aiodns/issues/86
+        logging.warning("aiodns is not needed on Windows and sometimes causes issues. You can safely uninstall it.")
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
     asyncio.run(main())
