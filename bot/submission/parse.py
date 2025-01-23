4c8ac2bb7705f8ff8488b5cab0f1a85d28daa83c
Glinte <96855131+Glinte@users.noreply.github.com>
2025-01-23 10:15:09 +0800

    Make parse_hallway_dimensions return -1 for known but unused dimensions

--- bot/submission/parse.py
+++ bot/submission/parse.py
@@ -191,11 +191,13 @@ def parse_dimensions(dim_str: str, *, min_dim: int = 2, max_dim: int = 3) -> tup
 def parse_hallway_dimensions(dim_str: str) -> tuple[int | None, int | None, int | None]:
     """Parses a string representing the door's <size>, which essentially is the hallway's dimensions.
 
+    None is used to represent a dimension that is not given. The value -1 is used to represent a dimension that is not applicable.
+
     Examples:
         "5x5x5" -> (5, 5, 5)
         "5x5" -> (5, 5, None)
-        "5 wide" -> (5, None, None)
-        "5 high" -> (None, 5, None)
+        "5 wide" -> (5, -1, -1)
+        "5 high" -> (-1, 5, -1)
 
     References:
         https://docs.google.com/document/d/1kDNXIvQ8uAMU5qRFXIk6nLxbVliIjcMu1MjHjLJrRH4/edit
@@ -203,19 +205,19 @@ def parse_hallway_dimensions(dim_str: str) -> tuple[int | None, int | None, int 
     Returns:
         A tuple of the dimensions (width, height, depth).
     """
+    if match := re.match(r"^(\d+)\s*(wide|high)$", dim_str):
+        size, direction = match.groups()
+        if direction == "wide":
+            return int(size), -1, -1
+        else:  # direction == "high"
+            return -1, int(size), -1
+
     try:
         return parse_dimensions(dim_str)
     except ValueError:
-        if match := re.match(r"^(\d+)\s*(wide|high)$", dim_str):
-            size, direction = match.groups()
-            if direction == "wide":
-                return int(size), None, None
-            else:  # direction == "high"
-                return None, int(size), None
-        else:
-            raise ValueError(
-                "Invalid hallway size. Must be in the format 'width x height [x depth]' or '<width> wide' or '<height> high'"
-            )
+        raise ValueError(
+            "Invalid hallway size. Must be in the format 'width x height [x depth]' or '<width> wide' or '<height> high'"
+        )
 
 
 async def main():
