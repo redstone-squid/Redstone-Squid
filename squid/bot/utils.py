0daa3947d8b5756f6d40b41f0762e9f501588a2f
Glinte <96855131+Glinte@users.noreply.github.com>
2025-01-27 12:05:13 +0800

    Implement sentinels

--- squid/bot/utils.py
+++ squid/bot/utils.py
@@ -3,11 +3,12 @@
 import asyncio
 import inspect
 import io
+import sys
 import logging
 import mimetypes
 from traceback import format_tb
 from types import FrameType, TracebackType
-from typing import Any, TypedDict
+from typing import Any, TypedDict, override
 
 import aiohttp
 import bs4
@@ -26,6 +27,69 @@ discord_green = 0x43B581
 
 
 logger = logging.getLogger(__name__)
+
+
+_registry = {}
+
+class Sentinel:
+    """Unique sentinel values."""
+    _name: str
+    _repr: str
+    _bool_value: bool
+    _module_name: str
+
+    def __new__(cls, name: str, repr: str | None = None, bool_value: bool = True, module_name: str | None = None):
+        repr = repr if repr else f'<{name.split(".")[-1]}>'
+        if module_name is None:
+            try:
+                module_name = sys._getframe(1).f_globals.get('__name__', '__main__')  # type: ignore
+            except (AttributeError, ValueError):
+                module_name = __name__
+            assert module_name is not None
+
+        registry_key = f'{module_name}-{name}'
+
+        sentinel = _registry.get(registry_key, None)
+        if sentinel is not None:
+            return sentinel
+
+        sentinel = super().__new__(cls)
+        sentinel._name = name
+        sentinel._repr = repr
+        sentinel._bool_value = bool_value
+        sentinel._module_name = module_name
+
+        return _registry.setdefault(registry_key, sentinel)
+
+    @override
+    def __eq__(self, other: object) -> bool:
+        return False
+
+    @override
+    def __repr__(self):
+        return self._repr
+
+    @override
+    def __hash__(self) -> int:
+        return 0
+
+    def __bool__(self):
+        return self._bool_value
+
+    @override
+    def __reduce__(self):
+        return (
+            self.__class__,
+            (
+                self._name,
+                self._repr,
+                self._module_name,
+            ),
+        )
+
+
+MISSING: Any = Sentinel('MISSING', repr="...")
+DEFAULT: Any = Sentinel('DEFAULT')
 
 
 def error_embed(title: str, description: str | None):
