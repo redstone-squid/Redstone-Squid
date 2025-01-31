1102c21e512b8e5f4884347ba3828468f23dcdd8
Glinte <96855131+Glinte@users.noreply.github.com>
2025-01-31 15:45:56 +0800

    Merge extensions

--- squid/bot/submission/__init__.py
+++ squid/bot/submission/__init__.py
@@ -1,1 +1,20 @@
 """Functions, UI components, and classes for handling submissions."""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from squid.bot.submission.edit import BuildEditCog
+from squid.bot.submission.search import SearchCog
+from squid.bot.submission.submit import BuildSubmitCog
+from squid.bot.submission.ui.components import DynamicBuildEditButton
+
+if TYPE_CHECKING:
+    import squid.bot
+
+
+async def setup(bot: squid.bot.RedstoneSquid) -> None:
+    """Called by discord.py when the cog is added to the bot via bot.load_extension."""
+    bot.add_dynamic_items(DynamicBuildEditButton)
+    await bot.add_cog(BuildEditCog(bot))
+    await bot.add_cog(SearchCog(bot))
+    await bot.add_cog(BuildSubmitCog(bot))
