559cacba5991e5b0a5833d40b27b5673f6648cb4
Glinte <96855131+Glinte@users.noreply.github.com>
2025-02-02 11:50:53 +0800

    Don't parse the edition when it is already given

--- squid/bot/version_tracking.py
+++ squid/bot/version_tracking.py
@@ -5,6 +5,7 @@ from typing import Literal
 import discord
 import discord.ext.commands as commands
 from discord.ext.commands import Cog
+from discord.ext.commands.bot import app_commands
 from postgrest.base_request_builder import APIResponse
 
 from squid.bot import RedstoneSquid
@@ -21,14 +22,15 @@ class VersionTracker[BotT: RedstoneSquid](Cog, name="VersionTracker"):
     @commands.hybrid_command()
     @check_is_staff()
     @check_is_owner_server()
+    @app_commands.rename(version_string="version")
     async def add_version(self, ctx: commands.Context, edition: Literal["Java", "Bedrock"], version_string: str):
         """Add a new version to the database"""
         db = DatabaseManager()
-        edition, major_version, minor_version, patch = parse_version_string(edition + version_string)
+        _, major, minor, patch = parse_version_string(version_string)
         version_record = {
             "edition": edition,
-            "major_version": major_version,
-            "minor_version": minor_version,
+            "major_version": major,
+            "minor_version": minor,
             "patch_number": patch,
         }
         response: APIResponse[VersionRecord] = await db.table("versions").insert(version_record).execute()
