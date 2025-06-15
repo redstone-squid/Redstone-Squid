777ae994c32e48cf15204104f22dddecad4f2d79
Glinte <96855131+Glinte@users.noreply.github.com>
2025-06-15 13:29:02 +0800

    Update database connection configuration and README for driver support

--- squid/db/__init__.py
+++ squid/db/__init__.py
@@ -8,7 +8,7 @@ import os
 from typing import ClassVar, Literal
 
 from async_lru import alru_cache
-from sqlalchemy import create_engine, select
+from sqlalchemy import create_engine, select, make_url
 from sqlalchemy.ext.asyncio import AsyncEngine, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
@@ -36,7 +36,9 @@ class DatabaseManager(AsyncClient):
         """Initializes the DatabaseManager with both Supabase and SQLAlchemy support."""
         supabase_url = supabase_url or os.environ.get("SUPABASE_URL")
         supabase_key = supabase_key or os.environ.get("SUPABASE_KEY")
-        database_url = database_url or os.environ.get("DB_CONNECTION")
+        database_url = database_url or os.environ.get("DATABASE_URL")
+        driver_sync = os.environ.get("DB_DRIVER_SYNC")
+        driver_async = os.environ.get("DB_DRIVER_ASYNC")
 
         if not supabase_url:
             raise RuntimeError(
@@ -53,6 +55,10 @@ class DatabaseManager(AsyncClient):
                 "database_url not given and no DATABASE_URL environmental variable found. "
                 "Specify DATABASE_URL either with a .env file or a DATABASE_URL environment variable."
             )
+        if not driver_sync:
+            raise RuntimeError("No DB_DRIVER_SYNC environment variable found.")
+        if not driver_async:
+            raise RuntimeError("No DB_DRIVER_ASYNC environment variable found.")
 
         # Initialize Supabase client
         super().__init__(supabase_url, supabase_key, options)
@@ -60,9 +66,10 @@ class DatabaseManager(AsyncClient):
         self.message = MessageManager(self)
 
         # Initialize SQLAlchemy engine and session maker
-        self.engine: AsyncEngine = create_async_engine(database_url, echo=False)
+        base = make_url(database_url)
+        self.engine = create_async_engine(base.set(drivername=f"{base.drivername}+{driver_async}"), echo=False)
         self.async_session = async_sessionmaker(self.engine, expire_on_commit=False)
-        self.sync_engine = create_engine(database_url, echo=False)
+        self.sync_engine = create_engine(base.set(drivername=f"{base.drivername}+{driver_sync}"), echo=False)
         self.sync_session = sessionmaker(self.sync_engine, expire_on_commit=False)
 
     def validate_database_consistency(self, base_cls: type[DeclarativeBase]) -> None:
