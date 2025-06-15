881d592fabff98117ac319618e6e3f04d0c3958d
Glinte <96855131+Glinte@users.noreply.github.com>
2025-06-15 22:56:22 +0800

    Update give_redstoner to add a button that removes your own role

--- squid/bot/give_redstoner.py
+++ squid/bot/give_redstoner.py
@@ -4,13 +4,56 @@
 from __future__ import annotations
 
 import re
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, override, Self, Any
 
 import discord
+from discord import Interaction
+from discord.ext import commands
 from discord.ext.commands import Cog
+from discord.ui import Item
 
 if TYPE_CHECKING:
     import squid.bot
+
+
+class DynamicRemoveOwnRedstonerRoleButton[BotT: squid.bot.RedstoneSquid, V: discord.ui.View](
+    discord.ui.DynamicItem[discord.ui.Button[V]], template=r"remove:role:redstoner:(\d+)"
+):
+    """A button that allows users to remove their own redstoner role."""
+    def __init__(self, user_id: int):
+        self.user_id = user_id
+        super().__init__(
+            discord.ui.Button(
+                label="I'm not a redstoner",
+                style=discord.ButtonStyle.red,
+                custom_id=f"remove:role:redstoner:{user_id}",
+            )
+        )
+
+    @classmethod
+    @override
+    async def from_custom_id(  # pyright: ignore [reportIncompatibleMethodOverride]
+        cls: type[Self], interaction: Interaction[BotT], item: Item[Any], match: re.Match[str], /
+    ) -> Self:
+        user_id = int(match.group(1))
+        return cls(user_id)
+
+    @override
+    async def callback(self, interaction: Interaction[BotT]) -> Any:  # pyright: ignore [reportIncompatibleMethodOverride]
+        await interaction.response.defer()
+        if interaction.user.id != self.user_id:
+            return
+
+        if interaction.guild is None or interaction.guild.id != interaction.client.owner_server_id:
+            return
+
+        member = interaction.guild.get_member(self.user_id)
+        if member is None:
+            return
+
+        redstoner_role = interaction.guild.get_role(433670432420397060)
+        if redstoner_role in member.roles:
+            await member.remove_roles(redstoner_role)
 
 
 class GiveRedstoner(Cog):
@@ -41,11 +84,15 @@ class GiveRedstoner(Cog):
         await message.channel.send(
             f"Gave {member.mention} the redstoner role.", allowed_mentions=discord.AllowedMentions.none()
         )
+
+        view = discord.ui.View()
+        view.add_item(DynamicRemoveOwnRedstonerRoleButton(member.id))
         await self.bot.get_channel(433643026204852224).send(
             f"Hi {member.mention}, you just got the {redstoner_role.mention} role because you received 15 upvotes in {orig_message_link}.",
-            allowed_mentions=discord.AllowedMentions(roles=False, users=(member,), everyone=False),
+            allowed_mentions=discord.AllowedMentions(roles=False, users=(member,), everyone=False), view=view
         )
 
 
-async def setup(bot: squid.bot.main.RedstoneSquid):
+async def setup(bot: squid.bot.RedstoneSquid):
+    bot.add_dynamic_items(DynamicRemoveOwnRedstonerRoleButton)
     await bot.add_cog(GiveRedstoner(bot))
