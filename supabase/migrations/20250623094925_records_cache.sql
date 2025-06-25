d22211c66494c50a77abe722f69f454433ca347d
Glinte <96855131+Glinte@users.noreply.github.com>
2025-06-25 18:42:26 +0800

    Fix refresh logic bug on INSERT

--- supabase/migrations/20250623094925_records_cache.sql
+++ supabase/migrations/20250623094925_records_cache.sql
@@ -344,9 +344,14 @@ BEGIN
     IF TG_OP = 'DELETE' THEN
         CALL public.refresh_smallest_after_door_delete(OLD.build_id);
 
-    ELSE -- INSERT OR UPDATE
+    ELSIF TG_OP = 'INSERT' THEN
+        -- remove the stale rows for *this* build first
         -- The reason why we need to delete the old winners even for INSERT is that
         -- here, INSERT can also mean "insert a new type/restriction" for an existing door,
+        CALL public.refresh_smallest_after_door_delete(NEW.build_id);
+        CALL public.refresh_smallest_for_door_insert(NEW.build_id);
+
+    ELSE -- UPDATE
         -- First remove the “old” winners, then add the “new” ones
         CALL public.refresh_smallest_after_door_delete(OLD.build_id);
         CALL public.refresh_smallest_for_door_insert(NEW.build_id);
