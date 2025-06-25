279356227522b9b40177bd4f74dcf665da9ad897
Glinte <96855131+Glinte@users.noreply.github.com>
2025-06-25 17:56:03 +0800

    Fix inserts not handled properly

--- supabase/migrations/20250623094925_records_cache.sql
+++ supabase/migrations/20250623094925_records_cache.sql
@@ -365,13 +365,13 @@ BEGIN
     IF TG_OP = 'DELETE' THEN
         CALL public.refresh_smallest_after_door_delete(OLD.build_id);
 
-    ELSIF TG_OP = 'UPDATE' THEN
+    ELSE -- INSERT OR UPDATE
+        -- The reason why we need to delete the old winners even for INSERT is that
+        -- here, INSERT can also mean "insert a new type/restriction" for an existing door,
         -- First remove the “old” winners, then add the “new” ones
         CALL public.refresh_smallest_after_door_delete(OLD.build_id);
         CALL public.refresh_smallest_for_door_insert(NEW.build_id);
 
-    ELSE   -- INSERT
-        CALL public.refresh_smallest_for_door_insert(NEW.build_id);
     END IF;
     RETURN NULL;
 END;
