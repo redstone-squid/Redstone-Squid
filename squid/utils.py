87837ede5f089ed51845d754ba4fb9e0f269ba80
Glinte <96855131+Glinte@users.noreply.github.com>
2025-07-10 22:14:48 +0800

    Add bg_set param to fire_and_forget

--- squid/utils.py
+++ squid/utils.py
@@ -27,11 +27,11 @@ def signature_from[**P, T](_original: Callable[P, T]) -> Callable[[Callable[P, T
     return _decorator
 
 
-def fire_and_forget(coro: Coroutine[None, None, Any]) -> None:
+def fire_and_forget(coro: Coroutine[None, None, Any], *, bg_set: set[asyncio.Task[Any] | Any] = _background_tasks) -> None:
     """Runs a coroutine in the background without waiting for it to finish."""
     task = asyncio.create_task(coro)
-    _background_tasks.add(task)
-    task.add_done_callback(_background_tasks.discard)
+    bg_set.add(task)
+    task.add_done_callback(bg_set.discard)
 
 
 async def _aiterator[T](it: Iterable[T]) -> AsyncIterator[T]:
