8c38ea6b0fc3f598f21170e83f26ef1422b3ca5c
Glinte <96855131+Glinte@users.noreply.github.com>
2025-07-11 09:05:34 +0800

    Rename worker.py to events_dispatcher.py

--- squid/bot/__init__.py
+++ squid/bot/__init__.py
@@ -122,7 +122,7 @@ class RedstoneSquid(Bot):
             "squid.bot.admin",
             "squid.bot.give_redstoner",
             "squid.bot.version_tracking",
-            "squid.bot.worker",
+            "squid.bot.events_dispatcher",
         ]
 
         await asyncio.gather(*(self.load_extension(ext) for ext in extensions))
