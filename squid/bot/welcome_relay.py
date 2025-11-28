0c726cfb52f0187cb03e447b517142f172985e1d
Glinte <96855131+Glinte@users.noreply.github.com>
2025-11-28 10:58:03 +0800

    Swap user names for a user mention

--- squid/bot/welcome_relay.py
+++ squid/bot/welcome_relay.py
@@ -1,5 +1,5 @@
 """Relay select welcome messages to a general discussion channel."""
-
+import asyncio
 import logging
 import random
 from typing import TYPE_CHECKING, Final
@@ -26,6 +26,11 @@ class WelcomeRelay[BotT: "squid.bot.RedstoneSquid"](Cog):
     def __init__(self, bot: BotT):
         self.bot = bot
         self.random = random.Random()
+        self.pending_members: dict[str, discord.Member] = {}
+        """Maps user names(!) to members who recently joined.
+        
+        This is because the discord welcome message only contains the user name, not the full member object.
+        """
 
     @Cog.listener(name="on_message")
     async def maybe_forward_welcome_message(self, message: discord.Message):
@@ -48,10 +53,23 @@ class WelcomeRelay[BotT: "squid.bot.RedstoneSquid"](Cog):
             logger.warning("General channel %s is not messageable", self.general_channel_id)
             return
 
+        await asyncio.sleep(5)  # Wait a bit to ensure the member is already cached from on_member_join
+        for member_name, member in self.pending_members.items():
+            if member_name in message.system_content:
+                break
+        else:
+            logger.warning("Could not find member for welcome message: %s", message.system_content)
+            return
+
         await general_channel.send(
-            message.system_content,
+            message.system_content.replace(member_name, member.mention),
             allowed_mentions=AllowedMentions(users=False, roles=False, everyone=False, replied_user=False),
         )
+
+    @Cog.listener(name="on_member_join")
+    async def track_new_member(self, member: discord.Member):
+        """Track a new member who joined, so we can match them to the welcome message later."""
+        self.pending_members[member.name] = member
 
 
 async def setup(bot: "squid.bot.RedstoneSquid"):
